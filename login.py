3b12895219a38630935390ee96819cae8e4cabbb
Lalit <laitkishore2005@gmail.com>
2026-01-05 13:28:42 +0530

    Added login feature

---
+++ login.py
@@ -0,0 +1,7 @@
+def login(username, password):
+    if username == "Lalit" and password == "1234":
+        print("Login successful")
+    else:
+        print("Invalid credentials")
+
+login("Lalit", "1234")
