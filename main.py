b7f04c3285c08d55237cbc5aa843e36f796a4cef
Lalit <laitkishore2005@gmail.com>
2026-01-05 21:22:35 +0530

    Changed registration message in main branch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def register_student(name, roll_no):
-    print(f"Student {name} with Roll No {roll_no} registered successfully")
+    print(f"Student {name} has Reg No {roll_no} registered ")
 
 register_student("Lalit", "23MIS0379")
